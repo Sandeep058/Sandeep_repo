011f674e1d08d6ca21a6236b89648128412d5344
Sandeep kumar <sandeepkumarcoool@gmail.com>
2021-10-31 15:04:12 +0530

    First

---
+++ LinkedList.cpp
@@ -0,0 +1,42 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+	int data;
+	Node* next;
+};
+
+void printlist(Node* n)
+{
+	while(n != NULL)
+	{
+		cout<<n->data<<" ";
+		n = n->next;
+	}
+}
+
+int main()
+{
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+
+	head = new Node();
+	second =new Node();
+	third = new Node();
+
+	head->data = 1;
+	head->next=second;
+
+	second->data=3;
+	second->next=third;
+
+	third->data=5;
+	third->next=NULL;
+
+	printlist(head);
+
+	return 0;
+}
+
